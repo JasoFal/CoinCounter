77ba2f4fc026f5dc7cbfa5c4dc6f867a148b3d50
Jason Falk <jasonfalk9@gmail.com>
2024-07-15 08:58:18 -0700

    Add dictionary to store change.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-let quarters = 0;
-let dimes = 0;
-let nickles = 0;
-const CoinCounter = (totalMoney) => {
+export const CoinCounter = (totalMoney, coinsObj = { "quarters" : 0, "dimes" : 0, "nickles" : 0, "pennies" : 0 }) => {
   if (isNaN(totalMoney)) {
     return null;
   }
+  totalMoney = totalMoney.toFixed(2);
+  if (totalMoney == 0)
+  {
+    return coinsObj;
+  }
+  // console.log(totalMoney, "totalMoney")
+  // console.log(coinsObj, "coinsObj");
   if (totalMoney >= .25) {
-    quarters ++;
-    return CoinCounter(totalMoney - .25);
+    coinsObj["quarters"]++;
+    return CoinCounter(totalMoney - .25, coinsObj);
   } else if (totalMoney >= .1) {
-    dimes ++;
-    return CoinCounter(totalMoney - .1)
+    coinsObj["dimes"] ++;
+    return CoinCounter(totalMoney - .1, coinsObj);
   } else if (totalMoney >= .05) {
-    nickles ++;
-    return CoinCounter(totalMoney - .05);
+    coinsObj["nickles"] ++;
+    return CoinCounter(totalMoney - .05, coinsObj);
+  } else if (totalMoney >= 0) {
+    coinsObj["pennies"] ++;
+    return CoinCounter(totalMoney - .01, coinsObj);
   }
 }
+
+// console.log(CoinCounter(5.99));
